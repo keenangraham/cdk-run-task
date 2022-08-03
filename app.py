a62b653d391ca341e2c74481825d037905a0c338
Keenan Graham <ksgraham@stanford.edu>
2022-08-03 14:49:58 -0700

    Add scheduled task

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from aws_cdk import App
+from aws_cdk import Duration
 from aws_cdk import Stack
 
 from constructs import Construct
@@ -8,6 +9,12 @@ from aws_cdk.aws_ecs import ContainerImage
 from aws_cdk.aws_ecs_patterns import QueueProcessingFargateService
 
 from aws_cdk.aws_applicationautoscaling import ScalingInterval
+
+from aws_cdk.aws_events import Rule
+from aws_cdk.aws_events import Schedule
+
+from aws_cdk.aws_events_targets import EcsTask
+from aws_cdk.aws_events_targets import ContainerOverride
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
 from shared_infrastructure.cherry_lab.vpcs import VPCs
@@ -43,6 +50,25 @@ class QueueStack(Stack):
                     change=2,
                 )
             ]
+        )
+
+        one_off_task_target = EcsTask(
+            cluster=fargate.cluster,
+            task_definition=fargate.task_definition,
+            role=fargate.task_definition.task_role,
+            container_overrides=ContainerOverride(
+                name='QueueProcessingContainer',
+                command=['echo', '$QUEUE_NAME']
+            )
+        )
+
+        Rule(
+            self,
+            'RunTask',
+            schedule=Schedule.rate(
+                Duration.minutes(1)
+            ),
+            targets=[one_off_task_target]
         )
 
 
